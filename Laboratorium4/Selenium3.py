dbba058363875051031b08548f900b619efa7be5
kn0w4k <knowacki.main@gmail.com>
2023-04-20 16:10:03 +0200

    Optimizing imports

--- Laboratorium4/Selenium3.py
+++ Laboratorium4/Selenium3.py
@@ -1,8 +1,9 @@
 import unittest
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class SecretSauceTest(unittest.TestCase):
